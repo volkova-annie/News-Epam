f1834bcb410d22740a05c6bcb456af6c9abe3a25
Anna Volkova <annie_v@bk.ru>
2017-12-04 23:24:49 +0300

    add webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+
+function resolve (dir) {
+  return path.join(__dirname, '.', dir)
+}
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'static')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: resolve('src'),
+        options: {
+          formatter: require('eslint-friendly-formatter'),
+          fix: true
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            'postcss-loader'
+          ]
+        })
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(['static']),
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      disable: false,
+      allChunks: true
+    }),
+    new webpack.ProvidePlugin({
+      fetch: 'imports-loader?this=>global!exports-loader?global.fetch!whatwg-fetch',
+      EventSource: 'imports-loader?this=>global!exports-loader?global.EventSource!event-source-polyfill'
+    })
+  ]
+}
