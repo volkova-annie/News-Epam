dcb9f8bfe18a200a511c00cd9bbdb38a1be207a4
Anna Volkova <annie_v@bk.ru>
2017-12-09 18:02:44 +0300

    add data from api and add styles

--- components/NewsList/index.js
+++ components/NewsList/index.js
@@ -1,19 +1,33 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import slug from 'slug'
+import {getDate} from '../modules/date.js'
 import './style.css'
 
 
+function findImage (multimedia) {
+  const image = multimedia.find((el) =>
+    (el.format &&  el.format === 'superJumbo'))
+  return image ? image.url : '/assets/no-photo.png'
+}
+
 const Item = props => {
   const id = slug(props.title).toLowerCase()
+  return <li className='post-item'>
+    <Link to={`/post/${id}`}  className='post-item__link'>
+    
+      <div className='image-wrapper'>
+        <img src={findImage(props.multimedia)} />
+      </div>
+      <span className='title'>{props.title}</span>
+      <span className='date'>{getDate(props.published_date)}</span>
 
-  return <li>
-    <Link to={`/post/${id}`}>{props.title}</Link>
+    </Link>
   </li>
 }
 
 const NewsList = props => {
-  return <ul className='newsList'>
+  return <ul className='news-list'>
     {props.news.map(item =>
       <Item
         key={item.url}
