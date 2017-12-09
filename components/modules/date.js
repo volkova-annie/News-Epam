2df588b3567b7c67f24acecf75291c5ffe46bd81
Anna Volkova <annie_v@bk.ru>
2017-12-09 22:32:53 +0300

    add condition to module date

--- components/modules/date.js
+++ components/modules/date.js
@@ -1,7 +1,12 @@
 export function getDate(date) {
-  const createdDate = new Date(date)
-  const mm = (createdDate.getMonth() + 1) >= 10 ? (createdDate.getMonth() + 1) : '0' + (createdDate.getMonth() + 1)
-  const dd = (createdDate.getDate()) >=10 ? (createdDate.getDate()) : '0' + (createdDate.getDate())
-  const yy = createdDate.getFullYear()
-  return  `${dd}.${mm}.${yy}`
+  if (date) {
+    const createdDate = new Date(date)
+    const mm = (createdDate.getMonth() + 1) >= 10 ? (createdDate.getMonth() + 1) : '0' + (createdDate.getMonth() + 1)
+    const dd = (createdDate.getDate()) >=10 ? (createdDate.getDate()) : '0' + (createdDate.getDate())
+    const yy = createdDate.getFullYear()
+    return  `${dd}.${mm}.${yy}`
+  }
+  else {
+    return ''
+  }
 }
