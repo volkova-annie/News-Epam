9436ab50fc9fa298eb6f18ab3cd56f9466678d86
Anna Volkova <annie_v@bk.ru>
2017-12-09 18:01:33 +0300

    add date module

---
+++ components/modules/date.js
@@ -0,0 +1,7 @@
+export function getDate(date) {
+  const createdDate = new Date(date)
+  const mm = (createdDate.getMonth() + 1) >= 10 ? (createdDate.getMonth() + 1) : '0' + (createdDate.getMonth() + 1)
+  const dd = (createdDate.getDate()) >=10 ? (createdDate.getDate()) : '0' + (createdDate.getDate())
+  const yy = createdDate.getFullYear()
+  return  `${dd}.${mm}.${yy}`
+}
