772693a73eb7c42586d1c3e3d6fdb7e9a6fd6cb3
Anna Volkova <annie_v@bk.ru>
2017-12-09 22:27:16 +0300

    modify action news

--- actions/news.js
+++ actions/news.js
@@ -3,35 +3,33 @@ import equal from 'equals'
 export function updateNews(news) {
   return {
     type: 'news/UPDATE_NEWS',
-    news: [...news]
+    news: [...news],
   }
 }
 
 export function requestNews () {
   return (dispatch, getState) => {
     const news = getState().news
-    const apiKey = '656af0c6ac43444b9bd39a0854759afa'
+    const apiKey = '4ed42e82763648ac9179da0d157d71bc'
     const url = 'https://api.nytimes.com/svc/topstories/v2/world.json?api-key=' + apiKey
+    // https://api.nytimes.com/svc/topstories/v2/world.json?api-key=656af0c6ac43444b9bd39a0854759afa
 
     fetch(url)
       .then(response => response.json())
       .then(response => {
         if (!equal(news, response.results)){
-          dispatch(updateNews(response.results))
+          const news = response.results.map((el) => {
+            el.published = new Date(el.published_date)
+
+            return el
+          })
+
+          dispatch(updateNews(news))
         }
       })
       .catch(error => {
         console.error(error) //eslint-disable-line no-console
       })
-  }
-}
-
-export function deleteOneNews (url) {
-  return (dispatch, getState) => {
-    const news = getState().news
-    const filteredNews = news.filter(item => item.url !== url)
-
-    dispatch(updateNews(filteredNews))
   }
 }
 
@@ -41,12 +39,15 @@ export function sortNews (event) {
     const news = getState().news
 
     news.sort((a, b) => {
-      const x = a[sorter] || ''
-      const y = b[sorter] || ''
-      if (x < y) return -1
-      if (x > y) return 1
-      return 0
-    })
+        const x = a[sorter] || '';
+        const y = b[sorter] || '';
+
+        if (typeof x === 'string') {
+          return  x.localeCompare(y)
+        }
+
+        return x-y;
+      })
 
     dispatch(updateNews(news))
   }
