3e464a33fc3c4c57abb5760a5656f025e7aa854e
Volkova Annie <annie_v@bk.ru>
2017-12-12 20:07:54 +0300

    remove console.log

--- reducers/search.js
+++ reducers/search.js
@@ -1,7 +1,6 @@
 export function search (state = {}, action) {
   switch (action.type) {
     case 'search/UPDATE_SEARCH': {
-      console.log(action);
       return {
         requested: !!action.requested,
         data: [...action.data],
