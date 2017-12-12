759e9966cdd81b1790b51e3fe725ee92069a443d
Volkova Annie <annie_v@bk.ru>
2017-12-12 21:15:08 +0300

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '.', dir)
@@ -51,6 +52,18 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(['static']),
+    // new HtmlWebpackPlugin({
+    //   filename: './index.html',
+    //   template: './server/templates/index.html',
+    //   inject: false,
+      // minify: {
+      //   removeComments: true,
+      //   collapseWhitespace: true,
+      //   removeAttributeQuotes: true
+      // },
+      // necessary to consistently work with multiple chunks via CommonsChunkPlugin
+      // chunksSortMode: 'dependency'
+    // }),
     new ExtractTextPlugin({
       filename: 'bundle.css',
       disable: false,
